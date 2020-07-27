29084cc66167cfcd75a6c7176473e1ac55905987
AAlmosto <amisto73@gmail.com>
2020-07-27 19:06:34 +0200

    feedback changes

--- Week1/createInsert.js
+++ Week1/createInsert.js
@@ -1,49 +1,50 @@
-const mySql = require('mysql');
+const mySql = require("mysql");
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'hyfuser',
-    password : 'hyfpassword',
-    database : 'meetup'
-  });
-  connection.connect(function(error){
-      if(error) throw error;
-      console.log('connection succeed')
-  });
-  connection.query("CREATE DATABASES meetup" ,function(error,result){
-      if(error){console.log(error)};
-      console.log('database created');
-  });
-  var createTables =[
-'CREATE TABLE Invitee(invitee_no init,invitee_name text,invited_by text)',
-'CREATE TABLE Room(room_no init , room_name text , floor_number init)',
-'CREATE TABLE Meeting(meeting_no init , meeting_title text , starting_time datetime, end_time datetime, room_no init)'
-];
-createTables.forEach(query =>{
-connection.query(query,function(err,result,field){
-    if(err) throw err;
-    console.log('table created')
-})
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "meetup",
 });
-var tableQuery=[
-"INSERT INTO Invitee VALUES(1,'Natasha','Hawkeye')",
-"INSERT INTO Invitee VALUES(2,'John Wick','Clark kent')",
-"INSERT INTO Invitee VALUES(3,'Peter Parker','Bruce Willes')",
-"INSERT INTO Invitee VALUES(4,'Tony Stark','Steve Rogers')",
-"INERT INTO Room VALUES(1,'Avengers meeting',4)",
-"INERT INTO Room VALUES(2,'Avengers Friends',3)",
-"INERT INTO Room VALUES(3,'Avengers planing room',5)",
-"INERT INTO Room VALUES(4,'Avenges control room',6)",
-"INSER INTO Meeting VALUES(1,'Avenger reunion','2019-12-11','2019-12-11',11)",
-"INSER INTO Meeting VALUES(2,'Avenger help center','2019-12-12','2019-12-12',12)",
-"INSER INTO Meeting VALUES(3,'Avengers','2019-12-13','2019-12-13',13)",
-"INSER INTO Meeting VALUES(4,'Avengers world protector','2019-12-14','2019-12-14',14)"
-];
-tableQuery.forEach(query=>{
-    connection.query(query,function(err,result){
-if(err) throw err;
-console.log(result);
-    })
+connection.connect(function (error) {
+  if (error) throw error;
+  console.log("connection succeed");
 });
-  
+connection.query("CREATE DATABASES meetup", function (error, result) {
+  if (error) {
+    console.log(error);
+  }
+  console.log("database created");
+});
+var createTables = [
+  "CREATE TABLE Invitee(invitee_no init,invitee_name text,invited_by text)",
+  "CREATE TABLE Room(room_no init , room_name text , floor_number init)",
+  "CREATE TABLE Meeting(meeting_no init , meeting_title text , starting_time datetime, end_time datetime, room_no init)",
+];
+createTables.forEach((query) => {
+  connection.query(query, function (err, result, field) {
+    if (err) throw err;
+    console.log("table created");
+  });
+});
+var tableQuery = [
+  "INSERT INTO Invitee VALUES(1,'Natasha','Hawkeye')",
+  "INSERT INTO Invitee VALUES(2,'John Wick','Clark kent')",
+  "INSERT INTO Invitee VALUES(3,'Peter Parker','Bruce Willes')",
+  "INSERT INTO Invitee VALUES(4,'Tony Stark','Steve Rogers')",
+  "INERT INTO Room VALUES(1,'Avengers meeting',4)",
+  "INERT INTO Room VALUES(2,'Avengers Friends',3)",
+  "INERT INTO Room VALUES(3,'Avengers planing room',5)",
+  "INERT INTO Room VALUES(4,'Avenges control room',6)",
+  "INSER INTO Meeting VALUES(1,'Avenger reunion','2019-12-11','2019-12-11',11)",
+  "INSER INTO Meeting VALUES(2,'Avenger help center','2019-12-12','2019-12-12',12)",
+  "INSER INTO Meeting VALUES(3,'Avengers','2019-12-13','2019-12-13',13)",
+  "INSER INTO Meeting VALUES(4,'Avengers world protector','2019-12-14','2019-12-14',14)",
+];
+tableQuery.forEach((query) => {
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 connection.end();
--- Week1/world.js
+++ Week1/world.js
@@ -1,63 +1,32 @@
-var mysql      = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'hyfuser',
-  password : 'hyfpassword',
-  database : 'world'
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "world",
 });
-connection.connect(function(error){
-    if(error) throw error;
-    console.log('connection succeed');
-});
-const population = 'SELECT name FROM country WHERE population >8000000';
-connection.query(population,(err,result)=>{
-if(err) throw err;
-console.log(result);
-});
-const landNames = "SELECT name FROM country WHERE name LIKE'%land%'";
-connection.query(landNames,(err,result)=>{
-if(err) throw err;
-console.log(result);
-});
-const cityName = "SELECT name FROM city WHERE population BETWEEN 500000 and 1000000";
-connection.query(cityName,(err,result)=>{
-if(err) throw err;
-console.log(result);
-});
-const europeCountries = "SELECT name FROM country WHERE continent ='Europe'";
-connection.query(europeCountries,(err,result)=>{
-if(err) throw err;
-console.log(result)
-});
-const orderSurfacearea ="SELECT name FROM country ORDER by surfacearea DESC";
-connection.query(orderSurfacearea,(err,result)=>{
-if(err) throw err;
-console.log(result)
-});
-const citiesNl = "SELECT name FROM city WHERE countrycode = 'NLD'";
-connection.query(citiesNl,(err,result)=>{
-if(err) throw err;
-console.log(result)
-});
-const populationRotterdam = "SELECT population FROM city WHERE name ='Rotterdam'";
-connection.query(populationRotterdam,(err,result)=>{
-    if(err) throw err;
-    console.log(result)
-});
-const surfaceArea_10 = "SELECT name FROM country ORDER by surfacearea DESC LIMIT 10";
-connection.query(surfaceArea_10,(err,result)=>{
-    if(err) throw err;
-    console.log(result)
-});
-const populationCities_10 = "SELECT name FROM city ORDER by population DESC LIMIT 10";
-connection.query(populationCities_10,(err,result)=>{
-if(err) throw err;
-console.log(result)
-});
-const worldPopulation = "SELECT SUM(population) FROM country";
-connection.query(worldPopulation,(err,result)=>{
-if(err) throw err;
-console.log(result)
+connection.connect(function (error) {
+  if (error) throw error;
+  console.log("connection succeed");
 });
 
+const query = [
+  "SELECT name FROM country WHERE population >8000000",
+  "SELECT name FROM country WHERE population >8000000",
+  "SELECT name FROM country WHERE name LIKE'%land%'",
+  "SELECT name FROM city WHERE population BETWEEN 500000 and 1000000",
+  "SELECT name FROM country WHERE continent ='Europe'",
+  "SELECT name FROM country ORDER by surfacearea DESC",
+  "SELECT name FROM city WHERE countrycode = 'NLD'",
+  "SELECT population FROM city WHERE name ='Rotterdam'",
+  "SELECT name FROM country ORDER by surfacearea DESC LIMIT 10",
+  "SELECT name FROM city ORDER by population DESC LIMIT 10",
+  "SELECT SUM(population) FROM country",
+];
+for (let i in query) {
+  connection.query(quer[i], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+}
 connection.end();
