6c9776f65c390ce3bb06bd5768aa19b16917e085
AAlmosto <amisto73@gmail.com>
2020-07-27 19:16:33 +0200

    feedback changes week2

--- Week2/relashionKeys.js
+++ Week2/relashionKeys.js
@@ -1,24 +1,24 @@
-const mysql = require('mysql');
-const util = require('util');
-const researchPaper = require('./values/research_paper');
-const authorResearch = require('./values/authorResearch')
+const mysql = require("mysql");
+const util = require("util");
+const researchPaper = require("./values/research_paper");
+const authorResearch = require("./values/authorResearch");
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'hyfuser',
-  password: '',
-  database: 'userdb',
-  multipleStatements: true
+  host: "localhost",
+  user: "hyfuser",
+  password: "",
+  database: "userdb",
+  multipleStatements: true,
 });
 const execQuery = util.promisify(connection.query.bind(connection));
 async function seedDataResearch() {
-  const create_table = `CREATE TABLE IF NOT EXITS Research_paper(
+  const researchPaper = `CREATE TABLE IF NOT EXITS Research_paper(
         paper_id INT,
         paper_title VARCHAR(50),
         conference VARCHAR(50),
         publish_date DATE
       )
       `;
-  const createTable = `
+  const AuthorPaper = `
       CREATE TABLE IF NOT EXIST ResearchAuthor(
         paper_id INT,
         author_no INT,
@@ -29,16 +29,20 @@ async function seedDataResearch() {
   connection.connect();
   try {
     await execQuery(create_table);
-    await execQuery(createTable);
-    await Promise.all(researchPaper.map(paper =>
-      execQuery('INSERT INTO authors SET ?', researchPaper)
-    ))
-    await Promise.all(authorResearch.map(research =>
-      execQuery('INSERT INTO authors SET ?', authorResearch)
-    ))
+    await execQuery(AuthorPaper);
+    await Promise.all(
+      researchPaper.map((paper) =>
+        execQuery("INSERT INTO authors SET ?", researchPaper)
+      )
+    );
+    await Promise.all(
+      authorResearch.map((research) =>
+        execQuery("INSERT INTO authors SET ?", authorResearch)
+      )
+    );
   } catch (error) {
-    console.error(error)
-    connection.end()
-  };
+    console.error(error);
+    connection.end();
+  }
 }
 seedDataResearch();
