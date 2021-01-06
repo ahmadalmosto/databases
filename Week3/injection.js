0a8c7fe0b71f8a023871d5892978809ba2afbcc7
AAlmosto <amisto73@gmail.com>
2021-01-06 13:36:16 +0100

    homework databases week2

---
+++ Week3/injection.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: '',
+  database: 'world',
+  multipleStatements: true
+});
+connection.connect(function(error){
+    if(error) throw error;
+    console.log('connection SUCCEED')
+    
+});
+function getPopulation(Country, name, code, cb) {
+   
+    connection.query(
+      `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = ${code}`,
+      function(err, result) {
+        if (err) cb(err);
+        if (result.length == 0) cb(new Error("Not found"));
+        cb(null, result[0].name);
+      }
+    );
+  }
+  function getPopulation2(Country, name, code, cb) {
+   
+    connection.query(
+      `SELECT Population FROM ?  WHERE Name = ?  and code = ? `,[Country ,name , code],
+      function(error, result) {
+        if (error) cb(error);
+        if (result.length == 0) cb(new Error("Not found"));
+        cb(null, result[0].name);
+      }
+    );
+  }
+  getPopulation2(country,Syria,SYR,(error,result)=>{
+if(error) throw error;
+console.log(result)
+  })
+
+  connection.end();
---
+++ Week3/tr-create-table.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: '',
+  database: 'userdb',
+  multipleStatements: true
+});
+connection.connect(function(error){
+    if(error) throw error;
+    console.log('connection SUCCEED')
+    
+});
+const execQuery = util.promisify(connection.query.bind(connection));
+async function seedData(){
+const accountTable = 
+`CREATE TABLE IF NOT EXIST account(
+   account_number INT,
+   balance DECIMAL,
+   CONSTRAINT PK_account PRIMARY KEY(account_number)
+  )`;
+  const accountChangesTable = 
+`CREATE TABLE IF NOT EXIST accountChanges(
+    change_number INT,
+    account_number INT,
+    amount INT,
+    changed_date DATETIME,
+    remark VARCHAR(50)
+  )`;
+  const AddforeignKey = `ALTER TABLE accountChangesTable ADD FOREIGN KEY(account_number) REFERENCES account(account_number)`;
+try{
+    await Promise.all[execQuery(accountTable),execQuery(accountChangesTable),
+    execQuery(AddforeignKey)];
+} catch(error){
+    console.error(error);  
+}
+connection.end();
+}
+seedData();
+  
+  
